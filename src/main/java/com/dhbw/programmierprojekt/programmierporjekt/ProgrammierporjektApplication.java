1a8efb7409bb6158fb415a9b41ec457e704dc00c
roukaula <roukaula@gmail.com>
2022-01-12 09:56:43 +0100

    initial commit

---
+++ src/main/java/com/dhbw/programmierprojekt/programmierporjekt/ProgrammierporjektApplication.java
@@ -0,0 +1,13 @@
+package com.dhbw.programmierprojekt.programmierporjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammierporjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammierporjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhbw/programmierprojekt/programmierporjekt/ProgrammierporjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhbw.programmierprojekt.programmierporjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammierporjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
